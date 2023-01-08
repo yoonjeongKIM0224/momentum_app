63b9bfdcab2f59c30a986f0a19cec8f9c083528a
gnoejnooy@gmail.com <gnoejnooy@gmail.com>
2023-01-09 02:17:24 +0900

    input 유효성 검사

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-// const loginForm = document.getElementById('login-form');
-// const loginInput = loginForm.querySelector('input');
-// const loginButton = loginForm.querySelector('button');
+/*
+const loginForm = document.getElementById('login-form');
+const loginInput = loginForm.querySelector('input');
+const loginButton = loginForm.querySelector('button');
+*/
+
+/*
+유효성 검사
+if(userName === ''){ //빈값일 경우
+    alert('please write your name.');
+} else if(userName.length > 15) { //글자가 15를 초과할 경우
+    alert('Your name is too long.');
+}
+*/
+
 
 const loginInput = document.querySelector('#login-form input');
 const loginButton = document.querySelector('#login-form button');
 
 function onLiginBtnClick (){
-    console.log(loginInput.value);
+    const userName = loginInput.value;
 }
 
 loginButton.addEventListener('click', onLiginBtnClick);
